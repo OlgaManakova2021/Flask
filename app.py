03781306dc9f35fa6f56149c8cc6fc06929705c8
OlgaManakova2021 <manakovaolga212@yandex.ru>
2025-05-01 20:54:47 +0300

    Добавили страницц приветствия

--- app.py
+++ app.py
@@ -4,7 +4,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Главная страница"
+    return """
+            <html>
+            <body>
+                <h1>Главная страница</h1>
+                <p> <font color='red'> Вы создали свой первый сайт!</font></p>
+                <a href="/hello/"> Перейти на страницу приветствия</a>
+            </body>
+            </html>
+            """
+
+@app.route('/hello/')
+def hello():
+
+    return """
+            <html>
+            <body>
+                <h1>Страница приветсвия</h1>
+                <p> <font color='red'> Что-бы перейти на Главную нажмите на ссылку ниже</font></p>
+                <a href="/"> Перейти на Главную</a>
+                <p> <font color='green'> Что-бы перейти к работе нажмите на ссылку ниже</font></p>
+                <a href="/"> Перейти на Главную</a>
+            </body>
+            </html>
+            """
+
+
 
 if __name__ == "__main__":
     app.run()
