1379e69a3d1a0b40515fce076f1936aab9e9927e
OlgaManakova2021 <manakovaolga212@yandex.ru>
2025-05-01 21:08:38 +0300

    Добавили temlates

--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """
-            <html>
-            <body>
-                <h1>Главная страница</h1>
-                <p> <font color='red'> Вы создали свой первый сайт!</font></p>
-                <a href="/hello/"> Перейти на страницу приветствия</a>
-            </body>
-            </html>
-            """
+    return render_template('index.html')
 
 @app.route('/hello/')
 def hello():
-
-    return """
-            <html>
-            <body>
-                <h1>Страница приветсвия</h1>
-                <p> <font color='red'> Что-бы перейти на Главную нажмите на ссылку ниже</font></p>
-                <a href="/"> Перейти на Главную</a>
-                <p> <font color='green'> Что-бы перейти к работе нажмите на ссылку ниже</font></p>
-                <a href="/"> Перейти на Главную</a>
-            </body>
-            </html>
-            """
+    return render_template("index_hello.html")
 
 
 
